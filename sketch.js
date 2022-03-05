2f02c412f2bb78673ab2af78ef2a72158b08ba3f
forkyspoon <david.guan@sjsu.edu>
2022-03-05 12:41:08 -0800

    added curated data

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  ellipse(100,100,100)
+  ellipse(300,100,100)
+  textSize(32);
+  textAlign(CENTER);
+  text('curated data', 200, 200);
+  
+  
+}

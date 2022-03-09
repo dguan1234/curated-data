18a6073a2c2e153c63526e08e951a096f654f876
forkyspoon <david.guan@sjsu.edu>
2022-03-08 19:52:27 -0800

    added random texts

--- sketch.js
+++ sketch.js
@@ -1,15 +1,47 @@
+//animals that we consume
+let animals = [{
+  name: "cows"
+}, {
+  name: "pigs"
+}, {
+  name: "chicken"
+}, {
+  name: "fish"
+}, {
+  name: "crab"
+}, {
+  name: "squid"
+}, {
+  name: "lobster"
+}];
+
+let randomIndex;
+
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  textSize(20);
+  push()
+  textSize(10)
+  text("Our servings to you in order", 200, 200);
+  pop()
+
+
 }
 
 function draw() {
-  background(220);
-  
-  ellipse(100,100,100)
-  ellipse(300,100,100)
-  textSize(32);
-  textAlign(CENTER);
-  text('curated data', 200, 200);
-  
-  
+
+
+}
+
+function mousePressed() {
+  if (animals[0]) {
+    background(random(255), random(255), random(255));
+    randomIndex = int(random(animals.length))
+    text(animals[randomIndex].name, 200, 200);
+    animals.splice(randomIndex, 1);
+  } else {
+    background(random(255), random(255), random(255));
+    text("enjoy", 200, 200);
+  }
 }
